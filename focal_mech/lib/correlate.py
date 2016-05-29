813ad20ee1a8c3cd035049853b0786be1d047469
Ben <Ben@blasscocks-MacBook-Pro.local>
2016-05-29 14:29:39 -0500

    Provides functions corr_shear and corr_tensile.
    These functions take the spectral content of a focal mechanism, and find the
    best orientation for the fault plane assuming shear or tensile sources.

---
+++ focal_mech/lib/correlate.py
@@ -0,0 +1,63 @@
+
+from numpy import (pi, rad2deg, inner, array, zeros, mgrid,
+                       argmin)
+from scipy.linalg import norm
+from scipy.optimize import minimize
+
+from focal_mech.lib.sph_harm import Wigner_d2
+
+
+def _corr_shear(x, alm):
+    """
+    Rotates the (13),(31) double couple source you can 
+    find in Ben-Menahem and Singh. Monopole, dipole 
+    terms for this source are zero.
+    """
+    
+    strike, dip, rake = x
+    # Wigner is ZYZ Euler rotation, \gamma = -rake
+    D = Wigner_d2(strike, dip, -rake).conjugate()
+    # Template Spectrum : (0, -1j, 0, -1j, 0)
+    prop = (inner(D[:,3], alm) + inner(D[:,1], alm))*-1j
+    # Maximize, not minimize.
+    return -norm(prop)
+
+def _scan_shear(alm):
+     """
+    :param: 
+
+    Scans for the best starting point, the optimization
+    can get stuck otherwise.
+    """
+   
+    # scan for a good starting point
+    X, Y, Z = mgrid[0:2*pi:10j, 0:pi:10j, 0:2*pi:10j]
+    x0s = zip(X.ravel(),Y.ravel(),Z.ravel())
+    res = [_corr_dc(x0, Alm[4:]) for x0 in x0s]
+    x0 = res[argmin(res)]
+
+    return x0
+    
+def corr_shear(Alm):
+    """
+    :param: 
+
+    Scans for the best starting point, the optimization
+    can get stuck otherwise.
+    """
+
+    # correlation with iso, dipole is zero.
+    alm = array([Alm[2,-2], Alm[2,-1],
+                 Alm[2,0],
+                 Alm[2,1],Alm[2,2]])    
+    # pick a good starting point.
+    x0 = _scan_shear(alm)
+
+    f = lambda x : _corr_dc(x,alm)
+    results = minimize(f, x0=x0,
+                    bounds=((0,2*pi), (0,pi), (0,2*pi)))
+
+    return rad2deg(results.x), results.fun
+    
+def corr_tensile(Alm):
+    raise Exception("Method not implemented")
