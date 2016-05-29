9737900256f2206242991730ac269f4b5e51818d
Ben <Ben@blasscocks-MacBook-Pro.local>
2016-05-29 16:08:45 -0500

    setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name = "focal_mech",
+      version = "0.1",
+      author = "Ben Lasscock",
+      author_email = "blasscoc@gmail.com",
+      description = ("An Earthquake classifier."),
+      packages=['focal_mech'],
+      install_requires = ['numpy', 'scipy', 'matplotlib',
+                          'mplstereonet',
+                          'obspy'
+                          'scikits-learn'])
+
+
