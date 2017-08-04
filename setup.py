b3eba82e87dd54cab2e1bc48852ba08284116609
Ben Lasscock <blasscock@sherman.geodev.com>
2017-08-04 09:14:10 -0500

    setup.py add find_packages.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name = "focal_mech",
       version = "0.1",
       author = "Ben Lasscock",
       author_email = "blasscoc@gmail.com",
       description = ("An Earthquake classifier."),
-      packages=['focal_mech'],
+      packages=find_packages(),
       install_requires = ['numpy', 'scipy', 'matplotlib',
                           'mplstereonet',
                           'obspy'])
